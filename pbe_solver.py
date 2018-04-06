fixed units

pbe_solver.py
@@ -161,11 +161,11 @@ def showData(zz, psi, pmf_an, pmf_cat, c_0, beta, valency, sigma_hat):
     rho_ion_n = 1E-27*c_0*np.exp(psi-pmf_an)  # anion density in nm^-3
     rho_ion_p = 1E-27*c_0*np.exp(-psi-pmf_cat)  # cation density in nm^-3
     # see if this balances out surface charge
-    dens_to_charge = sc.Avogadro*valency*1E-24
-    sig = np.trapz(dens_to_charge*(rho_ion_p - rho_ion_n), zz)  # excess surface charge
+    sig = np.trapz(valency*(rho_ion_p - rho_ion_n), zz)  # excess surface charge
 
+    sigma_units = np.sqrt(sc.epsilon_0*c_0)/(sc.elementary_charge*1E18*np.sqrt(beta))
     print("Surface charge: %.5f e/nm^2"
-          % (sigma_hat*np.sqrt(sc.epsilon_0*c_0)/np.sqrt(beta)))
+          % (sigma_hat*sigma_units))
     print("Excess System charge: %.5f e/nm^2" % sig)
 
     # convert psi to physical units [mV]