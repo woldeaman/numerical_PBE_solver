bbec73a531568d506abb7fb9ec60f674d8079cbf
woldeaman <amanuel.woldekidan@gmail.com>
2020-07-28 14:51:32 +0200

    now implemented correct support for asymmetric salts of ratios 1:2

--- pbe_solver/pbe_solver.py
+++ pbe_solver/pbe_solver.py
@@ -25,7 +25,7 @@ def parse_command_line():
                         'should match length of supplied profiles')
     parser.add_argument('-sig', dest='sigma', type=float, default=0,
                         help='surface charge on the walls [in e/nm^2]')
-    parser.add_argument('-z', dest='valency', type=int, default=1, nargs='*',
+    parser.add_argument('-z', dest='valency', type=int, default=[1], nargs='*',
                         help='valency of the ions\nif a list is supplied, first argument is cation valency')
     parser.add_argument('-c', dest='c_0', type=float, default=1,
                         help='bulk concentration of the ionic solution [in mol/l]')
@@ -80,15 +80,13 @@ def parse_command_line():
     eps, rho, pmf_cat, pmf_an = profiles[0], profiles[1], profiles[2], profiles[3]
     # set valencies for cation and anion seperately
     assert len(args.valency) <= 2, ('ERROR: List for ion valencies can only have two entries [val_cation, val_anion]!')
-    valency = [args.valency[0]]*2 if len(args.valency) == 1 else args.valency
-    # computing valency factors for concentration as well
-    val_cat, val_an = [[np.max(valency)/val, val] for val in valency]
+    val_cat, val_an = [args.valency[0]]*2 if len(args.valency) == 1 else args.valency
 
     return (path_out, verb, N, zz, eps, rho, pmf_cat, pmf_an, args.sigma,
             args.temp, args.distance, val_cat, val_an, args.c_0)
 
 
-def convert_units(bins, temp, dist, sig, rho, c_0):
+def convert_units(bins, temp, dist, sig, rho, valency, c_0):
     """
     Convert physical variables to dimensionless variables to solve PBE with.
     """
@@ -101,19 +99,19 @@ def convert_units(bins, temp, dist, sig, rho, c_0):
     rho = rho*sc.elementary_charge/(nm_to_m**3)  # external charge density in SI
 
     # compute rescaled variables
-    kappa = (sc.elementary_charge *  # modified debye length
+    kappa = (sc.elementary_charge * valency *  # modified debye length
              np.sqrt(beta*c_0/sc.epsilon_0))  # actual l_b would be sqrt(2/eps)*kappa
     zz_hat = np.linspace(0, dist/2, bins)*nm_to_m*kappa  # rescaled z-distance
     dz_hat = abs(zz_hat[0]-zz_hat[1])  # rescaled discretization width
     sigma_hat = sigma*np.sqrt(beta)/np.sqrt(sc.epsilon_0*c_0)  # rescaled surface charge
-    rho_hat = rho/(sc.elementary_charge*c_0)  # rescaled charge density
+    rho_hat = rho/(sc.elementary_charge*c_0*valency)  # rescaled charge density
 
     return (zz_hat, kappa, c_0, beta, dz_hat, sigma_hat, rho_hat)
 
 
 @jit(nopython=True)  # iterative loop compiled by numba
-def iteration_loop(psi_start, omega, dz_hat, val_cat, val_an, sigma_hat, rho_hat, eps,
-                   pmf_cat, pmf_an, tol=1E-10):
+def iteration_loop(psi_start, omega, dz_hat, val_cat, val_an, sigma_hat, rho_hat,
+                   eps, pmf_cat, pmf_an, tol=1E-10):
     # setting start values
     psi = psi_start
     N = psi.size  # gather number of discretization bins
@@ -124,7 +122,7 @@ def iteration_loop(psi_start, omega, dz_hat, val_cat, val_an, sigma_hat, rho_hat
 
     while rel_err > tol:  # iterate until convergence
         # start with value at left boundary, using neumann BCs
-        rho_0 = (val_cat[0]*np.exp(-val_cat[1]*psi[0]-pmf_cat[0]) - val_an[0]*np.exp(val_an[1]*psi[0]-pmf_an[0]) +
+        rho_0 = (np.exp(-val_cat*psi[0]-pmf_cat[0]) - np.exp(val_an*psi[0]-pmf_an[0]) +
                  rho_hat[0])
         psi_0 = (psi[1] +  # constant field due to surface charge bc
                  2*dz_hat*sigma_hat * (eps[1] + 3*eps[0])/8 +
@@ -133,7 +131,7 @@ def iteration_loop(psi_start, omega, dz_hat, val_cat, val_an, sigma_hat, rho_hat
 
         # now compute internal values inside domain
         for i in range(1, N-1):
-            rho_i = (val_cat[0]*np.exp(-val_cat[1]*psi[i]-pmf_cat[i]) - val_an[0]*np.exp(val_an[1]*psi[i]-pmf_an[i]) +
+            rho_i = (np.exp(-val_cat*psi[i]-pmf_cat[i]) - np.exp(val_an*psi[i]-pmf_an[i]) +
                      rho_hat[i])  # ion charge distribution + extra charges
             psi_i = (psi[i+1]*(-eps[i+1] + 4*eps[i] + eps[i-1])/(8*eps[i]) +
                      psi[i-1]*(eps[i+1] + 4*eps[i] - eps[i-1])/(8*eps[i]) +
@@ -141,7 +139,7 @@ def iteration_loop(psi_start, omega, dz_hat, val_cat, val_an, sigma_hat, rho_hat
             psi[i] = (1 - omega)*psi_prev[i] + omega*psi_i
 
         # now compute value at right boundary
-        rho_N = (val_cat[0]*np.exp(-val_cat[1]*psi[-1]-pmf_cat[-1]) - val_an[0]*np.exp(val_an[1]*psi[-1]-pmf_an[-1]) +
+        rho_N = (np.exp(-val_cat*psi[-1]-pmf_cat[-1]) - np.exp(val_an*psi[-1]-pmf_an[-1]) +
                  rho_hat[-1])
         psi_N = (psi[-2] +  # no field in bulk bc
                  rho_N * (dz_hat**2)*eps[-1]/2)
@@ -161,17 +159,18 @@ def iteration_loop(psi_start, omega, dz_hat, val_cat, val_an, sigma_hat, rho_hat
     return psi
 
 
-def showData(zz, psi, pmf_an, pmf_cat, c_0, beta, val_cat, val_an, sigma_hat, plot=False):
+def showData(zz, psi, pmf_an, pmf_cat, val_max, c_0, beta, val_cat, val_an, sigma_hat, plot=False):
     """
     Plots and prints computed data if in verbose mode.
     """
-
+    # compute cation and anion bulk density
+    c_cat, c_an = [c_0 if val_max == val else c_0*val_max/val for val in [val_cat, val_an]]
     # compute ion densities from potential and pmfs
-    rho_ion_p = 1E-27*c_0*val_cat[0]*np.exp(-val_cat[1]*psi-pmf_cat)  # cation density in nm^-3
-    rho_ion_n = 1E-27*c_0*val_an[0]*np.exp(val_an[1]*psi-pmf_an)  # anion density in nm^-3
+    rho_ion_p = 1E-27*c_cat*np.exp(-val_cat*psi-pmf_cat)  # cation density in nm^-3
+    rho_ion_n = 1E-27*c_an*np.exp(val_an*psi-pmf_an)  # anion density in nm^-3
 
     # convert psi to physical units [mV]
-    psi_to_phi = 1E3/(sc.elementary_charge*beta)
+    psi_to_phi = 1E3/(sc.elementary_charge*beta*val_max)
     # symmetrize profiles
     symm_psi = psi_to_phi*np.concatenate((psi, psi[::-1]))
     symm_dens_p = np.concatenate((rho_ion_p, rho_ion_p[::-1]))
@@ -181,7 +180,7 @@ def showData(zz, psi, pmf_an, pmf_cat, c_0, beta, val_cat, val_an, sigma_hat, pl
     if plot:
         make_plot(symm_zz, symm_psi, symm_dens_n, symm_dens_p)
         # see if ion charge density balances out surface charge
-        sig = np.trapz(rho_ion_p - rho_ion_n, zz)  # excess surface charge
+        sig = np.trapz(val_cat*rho_ion_p - val_an*rho_ion_n, zz)  # excess surface charge
         sigma_units = np.sqrt(sc.epsilon_0*c_0)/(sc.elementary_charge*1E18*np.sqrt(beta))
         print("Surface charge: %.5f e/nm^2" % (sigma_hat*sigma_units))
         print("Excess System charge: %.5f e/nm^2" % sig)
@@ -230,9 +229,11 @@ def main():
     # read from command line
     (path_out, verb, bins, zz, eps, rho, pmf_cat, pmf_an, sigma,
      temp, distance, val_cat, val_an, c_0_pre) = parse_command_line()
+    # largest valency is used for normalization
+    val_max = np.max([val_cat, val_an])
     # convert units
     (zz_hat, kappa, c_0, beta, dz_hat, sigma_hat, rho_hat) = convert_units(
-        bins, temp, distance, sigma, rho, c_0_pre)
+        bins, temp, distance, sigma, rho, val_max, c_0_pre)
 
     # compute gouy chapman solution to start with
     eps_avg = 1/np.average(eps)  # average epsilon
@@ -241,10 +242,10 @@ def main():
     omega = 2/(1 + np.sqrt(np.pi/bins))  # omega parameter for SOR
 
     # call iteration procedure
-    psi = iteration_loop(psi_start, omega, dz_hat, val_cat, val_an, sigma_hat, rho_hat,
-                         eps, pmf_cat, pmf_an)
+    psi = iteration_loop(psi_start, omega, dz_hat, val_cat, val_an,
+                         sigma_hat, rho_hat, eps, pmf_cat, pmf_an)
     (symm_zz, symm_psi,  # compute physical data and plot if in verbos mode
-     symm_dens_n, symm_dens_p) = showData(zz, psi, pmf_an, pmf_cat, c_0, beta,
+     symm_dens_n, symm_dens_p) = showData(zz, psi, pmf_an, pmf_cat, val_max, c_0, beta,
                                           val_cat, val_an, sigma_hat, plot=verb)
 
     # save computed potential and ion distributions
